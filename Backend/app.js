ed9f43fec8e76ceb9f7d0c025ea0f4167325a593
Anupama M T <anupamamadhu121@gmail.com>
2022-11-14 15:25:55 +0530

    Updated app.js
    changes

--- Backend/app.js
+++ Backend/app.js
@@ -3,18 +3,17 @@ const express=require('express')
 const cors=require('cors')
 const logger=require('morgan')
 const app=new express()
-app.use(cors())//connect frontend and backend seemlessly
+app.use(cors())
 app.use(express.json())
 // app.use(urlencoded({extended:true}))
-//recieve data from backend.otherwise undefined
-//images and files
+
 app.use(logger('dev'))
 
 const PORT=3000;
 
 
 
-// Task2: create mongoDB connection 
+
 const mongoose=require("mongoose")
 mongoose.connect('mongodb+srv://anupamamt:anupama2000@cluster0.nfdclmf.mongodb.net/BlogDB?retryWrites=true&w=majority')
 .then(()=>{
@@ -27,15 +26,7 @@ mongoose.connect('mongodb+srv://anupamamt:anupama2000@cluster0.nfdclmf.mongodb.n
 const BLOG_DETAILS=require('./models/model')
 
 
-// app.get('/aboutMaths',async(req,res)=>{
-//     try{
-// let blogName=aboutMaths;
-// const singlestudent=await BLOG_DETAILS.findOne(blogName)
-// res.send(singlestudent)
-//     }catch(error){
-//         console.log(error)
-//     }
-// })
+
 
 app.get('/aboutMaths',async(req,res)=>{
     try{
@@ -58,15 +49,7 @@ app.get('/aboutMaths',async(req,res)=>{
     }
   })
 
-app.delete('/aboutMaths/:id',async(req,res)=>{
-    try{
-        let id=req.params.id
-const deletestudent=await BLOG_DETAILS.findByIdAndDelete(id)
-res.send(deletestudent)
-    }catch(error){
-        console.log(error)
-    }
-})
+
   app.post('/aboutMaths',async(req,res)=>{
     try{
    let item={
@@ -78,9 +61,9 @@ res.send(deletestudent)
     comment:req.body.comment
 }
 
-const newstudent=new BLOG_DETAILS(item)//to check incoming data
-const savestudent=await newstudent.save()
-res.send(savestudent)
+const newblog=new BLOG_DETAILS(item)//to check incoming data
+const saveblog=await newblog.save()
+res.send(saveblog)
 
   }catch(error){
     console.log(error)
@@ -88,6 +71,6 @@ res.send(savestudent)
 })
 
 app.listen(PORT,()=>{
-    console.log("The app is listening to port 4200");
+    console.log("The app is listening to port 3000");
 })
 
